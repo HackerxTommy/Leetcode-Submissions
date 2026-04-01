d4d195fc4c438168f7e2d4bbc87922d91bdb3058
Vishal Gupta <153616435+HackerxTommy@users.noreply.github.com>
2026-04-02 04:54:41 +0530

    Time: 15 ms (17.22%) | Memory: 11.1 MB (18.06%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n=s.size();
+        int low=0,high=0, max_length=0, max_frequency=0;
+        unordered_map<char,int>mpp;
+        while(high<n){
+            mpp[s[high]]++; //current character ko map mai add karo
+            max_frequency= max(max_frequency,mpp[s[high]]); // jo element sabse jyada bar aaya ho most frequent  usko update karo
+
+            while ((high-low+1)-max_frequency>k){ //check if window is valid shrink the window if invalid
+                mpp[s[low]]--;
+                low++;
+            }
+           max_length= max(max_length,(high-low+1)); // update karo answer ko  har valid window pe
+          high++;
+        }
+         return max_length;
+    }
+   
+};
