70baedb9464bccf38d8ec0dd2e9cfc3b20d772a7
Vishal Gupta <153616435+HackerxTommy@users.noreply.github.com>
2026-04-16 03:45:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (17.11%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if(n<=1) return n;
-       vector<int>dp(n+1,-1);
-       //base case
-       dp[0]=1;
+    int climbStairs(int n) { //Tabulation approach bottom-up
+    //in tabulation we go from base cases to main problem
+
+        if(n<=1) return n; //if stair is 0 and stair is 1 then return 0&1 means n
+       vector<int>dp(n+1,-1); //initialise the dp of size n+1 with elements -1
+       //base cases
+       dp[0]=1;      
        dp[1]=1;
+       
        for(int i=2;i<=n;i++){
-       dp[i]= dp[i-1]+dp[i-2];
+       dp[i]= dp[i-1]+dp[i-2];  //recursive relation
        }
       return dp[n];
     }
