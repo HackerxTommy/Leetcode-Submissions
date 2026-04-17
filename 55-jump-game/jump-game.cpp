ee8af4adead1722f1ca72b7fb0bbb2fba63a9271
Vishal Gupta <153616435+HackerxTommy@users.noreply.github.com>
2026-04-18 00:43:41 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (56.45%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+           
+            if(i>res) return false;
+            res = max(i + nums[i], res);
+           
+            
+           
+           
+        }
+
+       return true;
+    }
+};
