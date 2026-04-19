6feb9c04250e7a6d01cc0e381e2f749aa38b9244
Vishal Gupta <153616435+HackerxTommy@users.noreply.github.com>
2026-04-20 02:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (87.12%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -2,12 +2,13 @@
 class Solution {
 public:
       int solve(int i, int j, int m, int n, vector<vector<int>>&dp){
-        //base case 
+        //base case
         if (i>=m || j>=n) return 0; //outof bounds
         if (i == m-1 && j == n-1) return dp[i][j] =1; // reached end pahuch gaya
         //pahle se calculate hua hai to usko return kardo
         if(dp[i][j]!=-1) return dp[i][j];
-// ab conditions lagao means operations like down to (i+1,j )row and right to (i,j+1) column
+// ab conditions lagao means operations like down to (i+1,j )row and right to
+(i,j+1) column
 
 return dp[i][j]=solve(i+1,j,m,n,dp)+ solve(i,j+1,m,n,dp);
 
@@ -17,33 +18,32 @@ return dp[i][j]=solve(i+1,j,m,n,dp)+ solve(i,j+1,m,n,dp);
 
 
     int uniquePaths(int m, int n) { //2d dp due to two operations
-        vector<vector<int>>dp(m, vector<int>(n,-1)); //2d dp m rows,n colums with -1 inside
-        return solve(0,0,m,n,dp);
+        vector<vector<int>>dp(m, vector<int>(n,-1)); //2d dp m rows,n colums
+with -1 inside return solve(0,0,m,n,dp);
     }
 };
 */
 
-// using memset 
+// using memset
 class Solution {
 public:
-int dp[105][105];
-      int solve(int i, int j, int m, int n){
-        //base case 
-        if (i>=m || j>=n) return 0; //outof bounds
-        if (i == m-1 && j == n-1) return dp[i][j] =1; // reached end pahuch gaya
-        //pahle se calculate hua hai to usko return kardo
-        if(dp[i][j]!=-1) return dp[i][j];
-// ab conditions lagao means operations like down to (i+1,j )row and right to (i,j+1) column
+    int dp[105][105];
+    int solve(int i, int j, int m, int n) {
+        // base case
+        if (i >= m || j >= n)
+            return 0; // outof bounds
+        if (i == m - 1 && j == n - 1)
+            return dp[i][j] = 1; // reached end pahuch gaya
+        // pahle se calculate hua hai to usko return kardo
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        // ab conditions lagao means operations like down to (i+1,j )row and
+        // right to (i,j+1) column
 
-return dp[i][j]=solve(i+1,j,m,n)+ solve(i,j+1,m,n);
-
-      }
-
-
-
-
-    int uniquePaths(int m, int n) { //2d dp due to two operations
-       memset(dp,-1,sizeof(dp));
-        return solve(0,0,m,n);
+        return dp[i][j] = solve(i + 1, j, m, n) + solve(i, j + 1, m, n);
+    }
+    int uniquePaths(int m, int n) { // 2d dp due to two operations
+        memset(dp, -1, sizeof(dp));
+        return solve(0, 0, m, n);
     }
 };
