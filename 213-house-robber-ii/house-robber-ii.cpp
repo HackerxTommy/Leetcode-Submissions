7c42a19b26cf738c1cdc25a1c9329055297910fe
Vishal Gupta <153616435+HackerxTommy@users.noreply.github.com>
2026-04-29 03:44:44 +0530

    Time: 3 ms (7.82%) | Memory: 10.9 MB (36.03%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     
     int solve(int i,int end, vector<int>&nums,vector<int>&dp){
+
        //base case
       if(i>end) return 0;
+
     //calculated check
     if(dp[i]!=-1) return dp[i];
+
     //condition check 
-    int rob = nums[i]+ solve(i+2,end,nums,dp);
-    int skip= solve(i+1,end,nums,dp);
+    //same logic like house robber 1 
+    int rob = nums[i]+ solve(i+2,end,nums,dp);  //if rob current house then dont take adjacent and move to i+2 but always consider end due to circular array
+    int skip= solve(i+1,end,nums,dp); // skip current house and consider end also
     return dp[i]= max(rob,skip);
    
 
@@ -17,9 +21,11 @@ public:
         int n=nums.size();
          vector<int>dp1(n,-1),dp2(n,-1);
         if (n==1) return nums[0];
-         
-        int case1= solve(0, n-2,nums, dp1);
-        int case2= solve(1,n-1,nums,dp2);
+         //ismai circular array hai then we have to consider end because 
+        //if i take 0 then 1 and n-1 are also adjacent 
+        
+        int case1= solve(0, n-2,nums, dp1); //so we take 0->n-2
+        int case2= solve(1,n-1,nums,dp2); // 1, n-1
         return max(case1, case2);
         
     }
