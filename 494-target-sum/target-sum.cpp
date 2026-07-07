705b8130ac500c6ecc7a11fc922e52aece3338bd
Vishal Gupta <153616435+HackerxTommy@users.noreply.github.com>
2026-07-07 14:12:41 +0530

    Time: 7 ms (66.57%) | Memory: 15.4 MB (48.57%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -3,11 +3,9 @@ public:
  int solve(  int i, vector<int>&nums,int x,int target, vector<vector<int>>&dp){
 
      //base case 
-     if(i== nums.size()){ 
+     if(i== nums.size()){  // last mai pahouch gaya then x=0 return got the answer
         return (x==0);
-       // if (x ==0 && nums[0]==0) return 2; // got sum=0 after recursion but nums=[0] then case +0,-0 -> 2
-      //  if( x==0 && x == nums[0]) return 1; //got sum answer is 1
-       // return 0; //else not got desired sum
+       
      }
    
      
@@ -29,7 +27,7 @@ public:
 
         int sum= accumulate(nums.begin(),nums.end(),0);
         if (sum < abs(target)) return 0;
-        if ((sum + target)%2) return 0;
+        if ((sum + target)%2!=0) return 0;
         int x= (sum+ target)/2; //cz p+n= sum and p-n= target then after adding them 2p=sum+target then p = (sum+target)/2
 
         vector<vector<int>>dp(n,vector<int>(x+1,-1)); 
