b5c7db31ecef8787fe85405397a02c125999617a
Vishal Gupta <153616435+HackerxTommy@users.noreply.github.com>
2026-08-20 19:49:41 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (63.89%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -8,7 +8,8 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+ */ 
+/* // using bfs
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
@@ -34,3 +35,25 @@ public:
         
     }
 };
+*/ 
+// using dfs
+class Solution {
+public:
+
+    void dfs( TreeNode * root, int level, vector<int> & ans){
+        if ( root == NULL) return;
+        if ( level == ans.size())
+        ans.push_back( root -> val);
+        dfs( root -> right, level+1,ans);
+        dfs( root -> left, level+1, ans);
+     
+    }
+
+
+    vector<int> rightSideView(TreeNode* root) {
+         vector<int> ans;
+         dfs( root, 0, ans);
+         return ans;
+
+    }
+    };
