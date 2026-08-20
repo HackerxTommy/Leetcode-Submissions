8c33d85a6fb960113b522f056015d65ed6b50bb9
Vishal Gupta <153616435+HackerxTommy@users.noreply.github.com>
2026-08-20 19:41:39 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (40.83%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -25,7 +25,7 @@ public:
             
                 if ( node -> left) q.push( node -> left);
                 if ( node -> right) q.push( node -> right);
-                if ( i== size-1)  level.push_back( node -> val);
+                if ( i== size-1)  level.push_back( node -> val); // for right side  and for left side i=0
             
         }
           ans.push_back( level[0]);
